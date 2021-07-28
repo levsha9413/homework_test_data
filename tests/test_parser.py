69a9e2bd8601f140f79f5c7654f904f090de43d1
Бочкарев Павел <pbochkarev@ussc.ru>
2021-07-28 22:20:08 +0500

    Удалил ненужные для домашки файлы

--- tests/test_parser.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def test_structure():
-    '''
-    Проверка того, что итоговый результат парсится библиотекой json
-    '''
-    with open("../result.json", "r") as result:
-        assert type(json.load(result)) == list
